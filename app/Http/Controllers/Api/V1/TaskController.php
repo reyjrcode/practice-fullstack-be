df17f1d36e1bea1646d26ad75a5b622f274e0e8c
ReyAlcala <rey.alcalajr.code@gmail.com>
2023-10-20 14:43:27 +0800

    update data

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTasksRequest;
+use App\Http\Requests\UpdateTaskRequest;
 use App\Http\Resources\TaskResource;
 use App\Models\Task;
 use Illuminate\Http\Request;
@@ -23,6 +24,13 @@ class TaskController extends Controller
     {
         //
         $task = Task::create($request->validated());
+
+        return TaskResource::make($task);
+    }
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        //
+        $task->update($request->validated());
 
         return TaskResource::make($task);
     }
