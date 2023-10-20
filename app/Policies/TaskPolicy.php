3e3d35c437a8a2d80dbb79b31291d483bac5ad92
ReyAlcala <rey.alcalajr.code@gmail.com>
2023-10-20 21:32:40 +0800

    authorization resources

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -15,10 +15,20 @@ class TaskPolicy
     {
         //
     }
-    
+
     public function view(User $user, Task $task): bool
     {
         //
+        return $user->id === $task->user_id;
+    }
+    public function update(User $user, Task $task): bool
+    {
+        //
+        return $user->id === $task->user_id;
+    }
+    public function delete(User $user, Task $task): bool
+    {
+
         return $user->id === $task->user_id;
     }
 }
