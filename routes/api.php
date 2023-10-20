6afe44b7da8281b8ff35fd26833001f346daa8bd
ReyAlcala <rey.alcalajr.code@gmail.com>
2023-10-20 20:43:31 +0800

    protecting routes

--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('v1')->group(function () {
-    Route::apiResource('tasks', TaskController::class);
+Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
+    Route::apiResource('/tasks', TaskController::class);
     Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
 });
 
