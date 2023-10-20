016a95f9c44179d9167f20fce7a68ac52c4661ab
ReyAlcala <rey.alcalajr.code@gmail.com>
2023-10-20 14:32:44 +0800

    working with eloquent API resource

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TaskResource;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
@@ -11,6 +12,10 @@ class TaskController extends Controller
     //
     public function index(Request $request)
     {
-        return Task::all();
+        return TaskResource::collection(Task::all());
+    }
+    public function show(Task $task)
+    {
+        return TaskResource::make($task);
     }
 }
