78cdcfaddbefe646f9d39d0e88003af21af1aed4
ReyAlcala <rey.alcalajr.code@gmail.com>
2023-10-20 14:24:42 +0800

    creating first endpoint

---
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    //
+    public function index(Request $request)
+    {
+        return Task::all();
+    }
+}
--- database/migrations/2023_10_20_061926_create_tasks_table.php
+++ database/migrations/2023_10_20_061926_create_tasks_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            // $table->string('10');
             $table->boolean('is_completed')->default(false);
             $table->timestamps();
         });
